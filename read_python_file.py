9b33381266575876bb58695cdfd937814ca536e7
Shahed808 <107167352+Shahed808@users.noreply.github.com>
2022-07-13 16:20:02 +0530

    Add files via upload

---
+++ read_python_file.py
@@ -0,0 +1,16 @@
+# f2=open('python_file_1.txt','r')
+# print(f2.read())                                                      # prints every thing that is stored in the create.py file
+# print(f2.read(5))                                                       # ''' p
+# print(f2.read(2))                                                       # ra
+# print(f2.read(3))                                                       # cti
+# print(f2.readline())
+# print(f2.readline())                                                      # ''' practising file handling system'''
+# print(f2.readline())                                                      # f1 = open('python_file_1.txt','x')
+# print(f2.readlines())                                                       # prints all the content stored in the create_file and prints in list form
+# d = f2.readlines
+# print(type(d))                                                              # <class 'builtin_function_or_method'>
+
+f3 = open('python_file_2.txt','r')
+for x in f3:
+    print(x)                                                                  # working on file handling operators 
+
